33878ecc15cf545d8ea63689e29bd540f0124630
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-17 15:58:25 -0700

    add reducers dir. with index.js reducer.

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { formVisibleReducer } from './form-visible-reducer';
+import { combineReducers } from 'redux';
+import { firestoreReducer } from 'react-redux-firebase';
+
+const rootReducer = combineReducers({
+  firestore: firestoreReducer
+});
+
+export default rootReducer;

66de3297f788c393fa138f193994526b277c7ce2
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-24 14:29:52 -0700

    add constructor to UserControl.js.

--- src/components/UserControl.js
+++ src/components/UserControl.js
@@ -3,5 +3,14 @@ import { connect } from 'react-redux';
 import { withFirestore, isLoaded } from 'react-redux-firebase';
 
 class UserControl extends React.Component {
-  constructor
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      selectedTicket: null,
+      editing:false
+    };
+  }
+
+  
 }

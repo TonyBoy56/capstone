f401bd82db02921ac08e81035d98179441062e4c
Antonio Cruz <codrocks.cruz3@gmail.com>
2020-07-24 11:50:46 -0700

    install call-of-duty-api through npm.

--- src/components/authorization/Signin.js
+++ src/components/authorization/Signin.js
@@ -30,6 +30,40 @@ function Signin() {
       console.log('Successfully signed out!');
     }).catch(function(error) {
       console.log(error.message);
-    })
+    });
   }
+
+  return (
+    <React.Fragment>
+      <h1>Sign Up</h1>
+      <form onSubmit = {doSignUp}>
+        <input
+          type = 'text'
+          name = 'email'
+          placeholder = 'Email' />
+        <input  
+          type = 'password'
+          name = 'password'
+          placeholder = 'Password' />
+        <button type = 'submit'>Sign Up</button>
+      </form>
+
+      <h1>Sign In</h1>
+      <form onSubmit = {doSignIn}>
+        <input
+          type = 'text'
+          name = 'signinEmail'
+          placeholder = 'Email' />
+        <input
+          type = 'password'
+          name = 'signinPassword'
+          placeholder = 'Password' />
+        <button type = 'submit'>Sign In</button>
+      </form>
+      <h1>Sign Out</h1>
+      <button onClick = {doSignOut}>Sign Out</button>
+    </React.Fragment>
+  );
 }
+
+export default Signin
